bbf5c2c283a4e895f4a24b458cded5ae0ecf6ac7
xcwstsoftware <xcwstsoftware@github.com>
2017-06-30 16:02:56 +0800

    modify sendmail2

--- sendmail2.py
+++ sendmail2.py
@@ -8,13 +8,19 @@ def mail():
   ret=True
   try:
     msg=MIMEText('填写邮件内容','plain','utf-8')
+    print("11ok")
     msg['From']=formataddr(["发件人邮箱昵称",my_sender])  #括号里的对应发件人邮箱昵称、发件人邮箱账号
+    print("22ok")
     msg['To']=formataddr(["收件人邮箱昵称",my_user])  #括号里的对应收件人邮箱昵称、收件人邮箱账号
+    print("33ok")
     msg['Subject']="主题" #邮件的主题，也可以说是标题
-
+    print("44ok")
     server=smtplib.SMTP("smtp.163.com",25) #发件人邮箱中的SMTP服务器，端口是25
-    server.login(my_sender,"wst365785")  #括号中对应的是发件人邮箱账号、邮箱密码
+    print("55ok")
+    server.login(my_sender,"wst1987")  #括号中对应的是发件人邮箱账号、邮箱密码
+    print("66ok")
     server.sendmail(my_sender,[my_user,],msg.as_string())  #括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+    print("77ok")
     server.quit()  #这句是关闭连接的意思
   except Exception:  #如果try中的语句没有执行，则会执行下面的ret=False
     ret=False
