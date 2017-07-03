9c328f2892e0869386ab2b13430b15ef020383a4
xcwstsoftware <xcwstsoftware@github.com>
2017-07-03 15:06:41 +0800

    sendemail modify

--- sendmail2.py
+++ sendmail2.py
@@ -20,24 +20,16 @@ my_user='584412572@qq.com' #收件人邮箱账号，为了后面易于维护，�
 def mail(to_my_user):
   ret=True
   try:
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject']="天猫内部优惠券" #邮件的主题，也可以说是标题
-    msgText=MIMEText('撒旦法第三方放水阀','plain','utf-8')
-    msgText['From']=formataddr(["tony",my_sender])  #括号里的对应发件人邮箱昵称、发件人邮箱账号
-    print("22okto_my_user=="+to_my_user)
-    msgText['To']=formataddr(["美女/帅哥",to_my_user])  #括号里的对应收件人邮箱昵称、收件人邮箱账号
-    msgRoot.attach(msgText)
-    fp = open('QR.png', 'rb')
-    msgImage = MIMEImage(fp.read())
-    fp.close()
-    msgImage.add_header('Content-ID', '')
-    msgRoot.attach(msgImage)
+    msg=MIMEText('填写邮件内容','plain','utf-8')
+    msg['From']=formataddr(["发件人邮箱昵称",my_sender])   #括号里的对应发件人邮箱昵称、发件人邮箱账号
+    msg['To']=formataddr(["收件人邮箱昵称",my_user])   #括号里的对应收件人邮箱昵称、收件人邮箱账号
+    msg['Subject']="主题" #邮件的主题，也可以说是标题
     print("44ok")
     server=smtplib.SMTP("smtp.163.com",25) #发件人邮箱中的SMTP服务器，端口是25
     print("55ok")
     server.login(my_sender,"wst1987")  #括号中对应的是发件人邮箱账号、邮箱密码
     print("66ok")
-    server.sendmail(my_sender,to_my_user,msgText.as_string())  #括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+    server.sendmail(my_sender,[my_user,],msg.as_string())  #括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
     print("77ok")
     server.quit()  #这句是关闭连接的意思
   except Exception:  #如果try中的语句没有执行，则会执行下面的ret=False
